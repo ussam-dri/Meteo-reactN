e65d09f752e163e323a8b5923fe26e80904d1458
Dabaghi-Anass <anass.debbaghi123@gmail.com>
2024-12-12 18:22:31 +0100

    ...

--- components/scroll-screen.tsx
+++ components/scroll-screen.tsx
@@ -6,7 +6,7 @@ import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 export default function ScrollScreen({ children, className, ...rest }: React.PropsWithChildren<ScrollViewProps>) {
   return (
     <SafeAreaProvider className='w-full h-full'>
-      <SafeAreaView >
+      <SafeAreaView className='h-full w-full'>
         <ScrollView contentContainerClassName={cn("h-full w-full", className)} {...rest}>
           {children}
         </ScrollView>
